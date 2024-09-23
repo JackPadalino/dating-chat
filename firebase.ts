6c513d41c79edec09e8901df9e36cb981a68c03a
JackPadalino <pada0867@gmail.com>
2024-09-23 13:46:50 -0400

    installed firebase

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAKaLl_nsGIWfaSks0VBi6v3Y99zcJ3gg",
+  authDomain: "dating-chat-a1047.firebaseapp.com",
+  projectId: "dating-chat-a1047",
+  storageBucket: "dating-chat-a1047.appspot.com",
+  messagingSenderId: "1018058573",
+  appId: "1:1018058573:web:70936eb6af4ae38325c904",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
