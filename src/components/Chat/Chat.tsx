e120d62ec02900aeeccf247a41085be35257f1e1
JackPadalino <pada0867@gmail.com>
2024-09-23 14:03:49 -0400

    added form to chat feature

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../firebase";
 
 const Chat = () => {
   const [user, setUser] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {
-      setUser(true);
-    } else {
-      setUser(false);
-    }
-  });
+  const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.currentTarget.value);
+    console.log(e.currentTarget.value);
+  };
+
+  const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(message);
+    setMessage("");
+  };
+
+  useEffect(() => {
+    const unsubmit = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(true);
+      } else {
+        setUser(false);
+      }
+    });
+    return () => unsubmit();
+  }, []);
 
   if (!user) return <p>Please sign in to join chat!</p>;
   return (
     <div>
-      <h1>This is the chat room component</h1>
+      <form onSubmit={handleSendMessage}>
+        <input
+          id="message"
+          value={message}
+          onChange={handleMessageChange}
+          placeholder="Say something nice..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
