e0d31ff29368e78b67d2a823a8d9516d5d600ca9
JackPadalino <pada0867@gmail.com>
2024-09-23 12:46:29 -0400

    installed react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./components";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={Router} />
+);
